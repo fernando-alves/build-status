ace22b99bd0ca3421266be18eb7201be55fb0738
Fernando <fernandoasfj@gmail.com>
2017-08-06 23:46:23 -0300

    Move config validation away from go

--- test/go/go.js
+++ test/go/go.js
@@ -2,25 +2,7 @@ const should = require('should');
 const Go = require('../../lib/go/go');
 
 describe('Go', () => {
-  describe('Configuration', () => {
-    it('throws error if mandatory fields are not present', () => {
-      const invalidConfig = {
-        username: 'user',
-        password: 'secret'
-      };
-
-      should(() => {
-        new Go(invalidConfig);
-      }).throw();
-    });
-  });
-
   it('lists all pipelines', done => {
-    const config = {
-      hostname: 'localhost',
-      username: 'user',
-      password: 'password'
-    };
     const allPipelines = ['api', 'coin'];
     const gateway = {
       pipelines: function(done) {
@@ -28,7 +10,7 @@ describe('Go', () => {
       }
     };
 
-    const go = new Go(config, gateway);
+    const go = new Go(gateway);
 
     go.pipelines((err, pipelines) => {
       should(pipelines).eql(allPipelines);
