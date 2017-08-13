da5c4c7efe58f38390f54cc832b8dabb37e9d65c
Fernando <fernandoasfj@gmail.com>
2017-08-12 22:32:12 -0300

    More details on index

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/* eslint no-console: "off" */
+
 const HttpClient = require('./lib/http/client');
 const Gateway = require('./lib/go/gateway');
 const Go = require('./lib/go/go');
@@ -7,4 +9,17 @@ const httpClient = new HttpClient(config);
 const gateway = new Gateway(httpClient);
 const go = new Go(gateway);
 
-go.pipelines(console.log); // eslint-disable-line
+const prettyPrint = pipeline => {
+  console.log(`Pipeline: ${pipeline.name}`);
+  pipeline.history.forEach(instance => {
+    console.log(`  Label: ${instance.label}\n  Result: ${instance.result}`);
+  });
+  console.log();
+};
+
+go.pipelines((err, pipelines) => {
+  if (err) {
+    throw err;
+  }
+  pipelines.forEach(prettyPrint);
+});
